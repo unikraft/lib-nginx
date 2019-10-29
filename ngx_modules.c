dbe21abcd7113894a1f8ff3e42c540af1d7e81ed
Mihai Pogonaru <pogonarumihai@gmail.com>
2019-10-29 22:56:35 +0200

    ngx_modules.c: Disable modules that are not available on Unikraft
    Signed-off-by: Mihai Pogonaru <pogonarumihai@gmail.com>
    Reviewed-by: Costin Lupu <costin.lupu@cs.pub.ro>

--- ngx_modules.c
+++ ngx_modules.c
@@ -7,10 +7,10 @@
 extern ngx_module_t  ngx_core_module;
 extern ngx_module_t  ngx_errlog_module;
 extern ngx_module_t  ngx_conf_module;
-extern ngx_module_t  ngx_regex_module;
+//extern ngx_module_t  ngx_regex_module;
 extern ngx_module_t  ngx_events_module;
 extern ngx_module_t  ngx_event_core_module;
-extern ngx_module_t  ngx_epoll_module;
+//extern ngx_module_t  ngx_epoll_module;
 extern ngx_module_t  ngx_select_module;
 extern ngx_module_t  ngx_http_module;
 extern ngx_module_t  ngx_http_core_module;
@@ -21,7 +21,7 @@ extern ngx_module_t  ngx_http_autoindex_module;
 extern ngx_module_t  ngx_http_index_module;
 extern ngx_module_t  ngx_http_mirror_module;
 extern ngx_module_t  ngx_http_try_files_module;
-extern ngx_module_t  ngx_http_auth_basic_module;
+//extern ngx_module_t  ngx_http_auth_basic_module;
 extern ngx_module_t  ngx_http_access_module;
 extern ngx_module_t  ngx_http_limit_conn_module;
 extern ngx_module_t  ngx_http_limit_req_module;
@@ -29,7 +29,7 @@ extern ngx_module_t  ngx_http_geo_module;
 extern ngx_module_t  ngx_http_map_module;
 extern ngx_module_t  ngx_http_split_clients_module;
 extern ngx_module_t  ngx_http_referer_module;
-extern ngx_module_t  ngx_http_rewrite_module;
+//extern ngx_module_t  ngx_http_rewrite_module;
 extern ngx_module_t  ngx_http_proxy_module;
 extern ngx_module_t  ngx_http_fastcgi_module;
 extern ngx_module_t  ngx_http_uwsgi_module;
@@ -47,7 +47,7 @@ extern ngx_module_t  ngx_http_write_filter_module;
 extern ngx_module_t  ngx_http_header_filter_module;
 extern ngx_module_t  ngx_http_chunked_filter_module;
 extern ngx_module_t  ngx_http_range_header_filter_module;
-extern ngx_module_t  ngx_http_gzip_filter_module;
+//extern ngx_module_t  ngx_http_gzip_filter_module;
 extern ngx_module_t  ngx_http_postpone_filter_module;
 extern ngx_module_t  ngx_http_ssi_filter_module;
 extern ngx_module_t  ngx_http_charset_filter_module;
@@ -62,10 +62,10 @@ ngx_module_t *ngx_modules[] = {
     &ngx_core_module,
     &ngx_errlog_module,
     &ngx_conf_module,
-    &ngx_regex_module,
+//    &ngx_regex_module,
     &ngx_events_module,
     &ngx_event_core_module,
-    &ngx_epoll_module,
+//    &ngx_epoll_module,
     &ngx_select_module,
     &ngx_http_module,
     &ngx_http_core_module,
@@ -76,7 +76,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_index_module,
     &ngx_http_mirror_module,
     &ngx_http_try_files_module,
-    &ngx_http_auth_basic_module,
+//    &ngx_http_auth_basic_module,
     &ngx_http_access_module,
     &ngx_http_limit_conn_module,
     &ngx_http_limit_req_module,
@@ -84,7 +84,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_map_module,
     &ngx_http_split_clients_module,
     &ngx_http_referer_module,
-    &ngx_http_rewrite_module,
+//    &ngx_http_rewrite_module,
     &ngx_http_proxy_module,
     &ngx_http_fastcgi_module,
     &ngx_http_uwsgi_module,
@@ -102,7 +102,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_header_filter_module,
     &ngx_http_chunked_filter_module,
     &ngx_http_range_header_filter_module,
-    &ngx_http_gzip_filter_module,
+//    &ngx_http_gzip_filter_module,
     &ngx_http_postpone_filter_module,
     &ngx_http_ssi_filter_module,
     &ngx_http_charset_filter_module,
@@ -119,10 +119,10 @@ char *ngx_module_names[] = {
     "ngx_core_module",
     "ngx_errlog_module",
     "ngx_conf_module",
-    "ngx_regex_module",
+//    "ngx_regex_module",
     "ngx_events_module",
     "ngx_event_core_module",
-    "ngx_epoll_module",
+//    "ngx_epoll_module",
     "ngx_select_module",
     "ngx_http_module",
     "ngx_http_core_module",
@@ -133,7 +133,7 @@ char *ngx_module_names[] = {
     "ngx_http_index_module",
     "ngx_http_mirror_module",
     "ngx_http_try_files_module",
-    "ngx_http_auth_basic_module",
+//    "ngx_http_auth_basic_module",
     "ngx_http_access_module",
     "ngx_http_limit_conn_module",
     "ngx_http_limit_req_module",
@@ -141,7 +141,7 @@ char *ngx_module_names[] = {
     "ngx_http_map_module",
     "ngx_http_split_clients_module",
     "ngx_http_referer_module",
-    "ngx_http_rewrite_module",
+//    "ngx_http_rewrite_module",
     "ngx_http_proxy_module",
     "ngx_http_fastcgi_module",
     "ngx_http_uwsgi_module",
@@ -159,7 +159,7 @@ char *ngx_module_names[] = {
     "ngx_http_header_filter_module",
     "ngx_http_chunked_filter_module",
     "ngx_http_range_header_filter_module",
-    "ngx_http_gzip_filter_module",
+//    "ngx_http_gzip_filter_module",
     "ngx_http_postpone_filter_module",
     "ngx_http_ssi_filter_module",
     "ngx_http_charset_filter_module",
